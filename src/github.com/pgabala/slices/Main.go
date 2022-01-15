417eee72f29f654d02ae356951a01f0aab08f5a1
Patrick <pgabala@bgsu.edu>
2022-01-15 17:45:19 -0500

    more notes

---
+++ src/github.com/pgabala/slices/Main.go
@@ -0,0 +1,32 @@
+package main
+
+import "fmt"
+
+var (
+	i int = 27
+)
+
+func main() {
+	grades := []int{97, 85, 93, 90, 95}
+
+	fmt.Printf("Grades: %v\n", grades)
+	fmt.Printf("Amount of grades, \n", len(grades))
+	fmt.Printf("Capacity of grades, \n", cap(grades))
+	fmt.Printf("\n")
+	// slicing operations
+
+	a := grades[:]   // slice of all elements
+	b := grades[3:]  // slice from the 4th element to the end
+	c := grades[:3]  // slice of first two elements
+	d := grades[1:4] // slice of 2nd, 3rd, and 4th elements
+
+	fmt.Println(a)
+	fmt.Println(b)
+	fmt.Println(c)
+	fmt.Println(d)
+
+	x := make([]int, 3)
+	fmt.Println(x)
+	fmt.Printf("Length: %v\n", len(x))
+	fmt.Printf("Capacity: %v\n", cap(x))
+}
